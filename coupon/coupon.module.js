090993eb9b7968005e4958001418920b6ddb8784
Kasun <kasun.w@duosoftware.com>
2017-06-06 13:03:20 +0530

    Version 6.1.0.4

--- coupon/coupon.module.js
+++ coupon/coupon.module.js
@@ -1,8 +1,8 @@
 //////////////////////////////////////
 // App : Coupon
 // Owner  : Ishara Gunathilaka
-// Last changed date : 2017/05/05
-// Version : 6.1.0.3
+// Last changed date : 2017/06/06
+// Version : 6.1.0.4
 // Modified By : Kasun
 /////////////////////////////////
 
