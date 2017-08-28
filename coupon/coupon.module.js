c398e5c7217a915cb6cf4ee2d4c0c936375ad26a
Kasun <kasun.w@duosoftware.com>
2017-08-28 17:14:10 +0530

    Revert test code

--- coupon/coupon.module.js
+++ coupon/coupon.module.js
@@ -2,7 +2,7 @@
 // App : Coupon
 // Owner  : Ishara Gunathilaka
 // Last changed date : 2017/08/24
-// Version : 6.1.0.13
+// Version : 6.1.0.12
 // Modified By : Kasun
 /////////////////////////////////
 
@@ -17,21 +17,6 @@
     /** @ngInject */
     function config($stateProvider, msNavigationServiceProvider, mesentitlementProvider)
     {
-        function gst(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            //debugger;
-            return null;
-        }
-        /** Check for Super admin */
-        var isSuperAdmin = gst('isSuperAdmin');
-        /** Check for Super admin - END */
-
         $stateProvider
             .state('app.coupon', {
                 url    : '/coupon',
@@ -45,8 +30,7 @@
 					security: ['$q','mesentitlement','$timeout','$rootScope','$state','$location', function($q,mesentitlement,$timeout,$rootScope,$state, $location){
 						return $q(function(resolve, reject) {
 							$timeout(function() {
-                $rootScope.isBaseSet2 = true;
-								if ($rootScope.isBaseSet2 && isSuperAdmin != 'true') {
+								if ($rootScope.isBaseSet2) {
 									resolve(function () {
 										var entitledStatesReturn = mesentitlement.stateDepResolver('coupon');
 
